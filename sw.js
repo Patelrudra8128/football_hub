7fd2c6c50cf58b43deb0b7646a68f74439706caf
Rudra Patel <126685264+Patelrudra8128@users.noreply.github.com>
2026-06-16 21:27:18 +0530

    Edited sw.js file for monetag push ads

--- sw.js
+++ sw.js
@@ -4,3 +4,11 @@ self.options = {
 }
 self.lary = ""
 importScripts('https://5gvci.com/act/files/service-worker.min.js?r=sw')
+
+
+self.options = {
+    "domain": "5gvci.com",
+    "zoneId": 11157509
+}
+self.lary = ""
+importScripts('https://5gvci.com/act/files/service-worker.min.js?r=sw')
