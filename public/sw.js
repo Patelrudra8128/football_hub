47b966dc1c37bfd18ad2958284ede30734a383c0
Rudra Patel <126685264+Patelrudra8128@users.noreply.github.com>
2026-06-17 20:51:19 +0530

    Add monetag

--- public/sw.js
+++ public/sw.js
@@ -1,6 +1,6 @@
 self.options = {
-    "domain": "5gvci.com",
-    "zoneId": 11157509
+    "domain": "3nbf4.com",
+    "zoneId": 11162488
 }
 self.lary = ""
-importScripts('https://5gvci.com/act/files/service-worker.min.js?r=sw')
+importScripts('https://3nbf4.com/act/files/service-worker.min.js?r=sw')
