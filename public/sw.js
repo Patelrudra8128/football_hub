96de53cf96378844be091d7b569a2ab8300a3d18
Rudra Patel <126685264+Patelrudra8128@users.noreply.github.com>
2026-06-17 21:38:11 +0530

    Replace monetag file

--- public/sw.js
+++ public/sw.js
@@ -1,6 +1,6 @@
 self.options = {
     "domain": "3nbf4.com",
-    "zoneId": 11162488
+    "zoneId": 11162701
 }
 self.lary = ""
 importScripts('https://3nbf4.com/act/files/service-worker.min.js?r=sw')
